687ca4f1950c6943c88ab5b92253e6eac94b2403
Butakyr <burulcampbell@gmail.com>
2025-03-13 22:49:33 -0400

    Update HelloProject.java

--- src/main/java/day01/HelloProject.java
+++ src/main/java/day01/HelloProject.java
@@ -4,5 +4,9 @@ public class HelloProject {
     public static void main(String[] args) {
 
         System.out.println("This is my project");
+        System.out.println("This is to practice");
+        
+
+       //Some changes are added
     }
 }
